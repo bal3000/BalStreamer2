7e010c7662cf7e05dffe8c6fd65ac1339267ae5c
bal3000 <bal3000286@googlemail.com>
2021-06-01 16:12:29 +0100

    fixed issues on streams page

--- client/pages/live-fixtures/[timerId].tsx
+++ client/pages/live-fixtures/[timerId].tsx
@@ -1,42 +1,58 @@
 import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
 
-import { useTypedSelector } from '../../hooks';
+import { useTypedSelector, useActions } from '../../hooks';
 import StreamOverview from '../../components/streams/stream-overview';
 import StreamDetails from '../../components/streams/stream-details';
 import streamerApi from '../../helpers/api-caller';
 import { Streams } from '../../models/streams';
+import { LiveFixture } from '../../models/live-fixture';
+import { SportType } from '../../models/sport-type.enum';
 
 interface LiveFixtureDetailsProps {
   streams: Streams;
+  currentFixture: LiveFixture;
 }
 
 export const getServerSideProps: GetServerSideProps<LiveFixtureDetailsProps> =
   async ({ query }) => {
+    const today = new Date().toISOString().split('T')[0];
     const { data: streams } = await streamerApi.get<Streams>(
       `/api/livestreams/${query.timerId}`
     );
+    const { data: fixtures } = await streamerApi.get<LiveFixture[]>(
+      `/api/livestreams/${SportType.All}/${today}/${today}`
+    );
 
-    console.log(streams);
+    const currentFixture = fixtures.find(
+      (f) => f.timerId === query.timerId && f.isPrimary === 'true'
+    );
+
+    if (!currentFixture) {
+      return {
+        notFound: true,
+      };
+    }
 
     return {
       props: {
         streams,
+        currentFixture,
       },
     };
   };
 
 export default function LiveFixtureDetails({
   streams,
+  currentFixture,
 }: LiveFixtureDetailsProps) {
-  const router = useRouter();
   const fixture = useTypedSelector(({ fixture }) => fixture?.selectedFixture);
   const selectedChromecast = useTypedSelector(
     ({ chromecasts }) => chromecasts?.selectedChromecast
   );
+  const { selectFixture } = useActions();
 
-  // TODO:  if null e.g. direct link, call api and re fetch data and set the selected fixture
-  if (!fixture) {
+  if (!fixture && currentFixture) {
+    selectFixture(currentFixture);
   }
 
   const castStream = async () => {
@@ -45,7 +61,7 @@ export default function LiveFixtureDetails({
       streamURL: streams.rtmp,
     });
 
-    router.push('/');
+    window.location.href = '/';
   };
 
   return (
